6a630765bf2b1a29a7b636783f3357caad7777b6
Ian Be <ian84be@gmail.com>
2019-04-09 18:27:49 -0500

    track innings/outs & test

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -5,6 +5,8 @@ class App extends Component {
   state = {
     balls: 0,
     strikes: 0,
+    inning: 1,
+    outs: 0
   }
   render() {
     return (
@@ -12,6 +14,8 @@ class App extends Component {
         <div className="Display">
         <h2>Balls: {this.state.balls}</h2>
         <h2>Strikes: {this.state.strikes}</h2>
+        <h2>Inning: {this.state.inning}</h2>
+        <h2>Outs: {this.state.outs}</h2>
         </div>
 
         <div className="Dashboard">
@@ -23,33 +27,57 @@ class App extends Component {
       </div>
     );
   }
+
+  handleOuts() {
+    let {outs,inning} = this.state;
+    if (outs < 2) {
+      this.setState({
+        ...this.state,
+        balls: 0,
+        strikes: 0,
+        outs: outs +=1
+      });
+    } else if (outs === 2 && inning < 9) {
+      this.setState({
+        ...this.state,
+        balls: 0,
+        strikes: 0,
+        inning: inning +=1,
+        outs: 0,
+      });
+    } else if (outs === 2 && inning === 9) {
+      this.setState({
+        ...this.state,
+        inning: 'Game Over!'
+      });
+    }
+  }
+
+  //BUTTON METHODS
   strike = e => {
     let {strikes} = this.state;
-    if (strikes < 3) {
+    if (strikes < 2) {
       this.setState({
         ...this.state,
         strikes: strikes+=1
-      })
+      });
     } else {
-      this.setState({
-        ...this.state,
-        strikes: 0,
-      })
+      this.handleOuts();
     }
   }
 
   ball = e => {
     let {balls} = this.state;
-    if (balls < 4) {
+    if (balls < 3) {
       this.setState({
         ...this.state,
         balls: balls+=1
-      })
+      });
     } else {
       this.setState({
         ...this.state,
         balls: 0,
-      })
+      });
     }
   }
 
@@ -59,7 +87,7 @@ class App extends Component {
       this.setState({
         ...this.state,
         strikes: strikes+=1
-      })
+      });
     }
   }
 
@@ -68,9 +96,8 @@ class App extends Component {
       ...this.state,
       balls: 0,
       strikes: 0
-    })
+    });
   }
 
 } // APP
-
 export default App;
